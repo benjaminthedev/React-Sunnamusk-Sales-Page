ee1e12ec700042d78f9a6af627d9742c6d8819f0
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-11-29 07:25:25 +0000

    updated

---
+++ application/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Product from './Components/ProductSection/Product';
+import './App.scss'
+
+function App() {
+  return (
+    <div>
+      <Header />
+       <Product />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ application/src/Components/Header/FindAStore.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FindAStore = () => {
+    return (
+        <div>
+            <a href="https://sunnamusk.com/find-a-store/" class="findAStore" target="_blank" rel="noreferrer">
+            <img src="https://sunnamusk.com/wp-content/themes/sunnamusk/dist/icons/nav-pin.svg" alt="Find a Sunnamusk Store" className="findIcon"/> Find a store</a>
+        </div>
+    )
+}
+
+export default FindAStore
